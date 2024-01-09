514d9471ae3a07196d63a5e8323ce83fb562e62d
Aiara Lobo Gomes <aiara.lobogomes@maastro.nl>
2024-01-09 14:59:47 +0100

    Partitioning data for testing

--- vtg_km_he/example.py
+++ vtg_km_he/example.py
@@ -1,8 +1,21 @@
-from vantage6.tools.mock_client import ClientMockProtocol
-import vtg_km_he
-import numpy as np
+import os
 import warnings
+import vtg_km_he
+import pandas as pd
+from vantage6.tools.mock_client import ClientMockProtocol
 warnings.filterwarnings("ignore")
+
+# Partitioning data
+data_dir = os.path.join(os.getcwd(), "vtg_km_he", "local")
+data_path = os.path.join(data_dir, "data_test.csv")
+data1_path = os.path.join(data_dir, "data1.csv")
+data2_path = os.path.join(data_dir, "data2.csv")
+if not os.path.exists(data1_path):
+    df = pd.read_csv(data_path)
+    df = df.sample(frac=1) # Shaffle data
+    df[0:int(len(df)/2)].to_csv(data1_path)
+    df[int(len(df)/2):].to_csv(data2_path)
+
 
 # Initialize the mock server. The datasets simulate the local datasets from
 # the node. In this case we have two parties having two different datasets:
@@ -10,9 +23,10 @@ warnings.filterwarnings("ignore")
 # package. This is the name you specified in `setup.py`, in our case that
 # would be v6-correlation-matrix-py.
 client = ClientMockProtocol(
-    datasets=["./local/data_test.csv", "./local/data_test.csv"],
+    datasets=[os.path.join(data_dir, "data1.csv"),
+              os.path.join(data_dir, "data2.csv")],
 #    datasets=["./local/TR_Maastricht_data (1).csv", "./local/TR_Maastricht_data (1).csv"],
-    module="vtg_km"
+    module="vtg_km_he"
 )
 
 
@@ -28,13 +42,15 @@ average_task = client.create_new_task(
     input_={
         'master': 1,
         'method': 'master',
-        'kwargs': {'time_col': 'T','censor_col':'C','data_set': 'all' } #'coef':[{'0': 0.1, '1': 0.2, '2': -0.1}]
+        'kwargs': {'time_col': 'T','censor_col':'C'}#,'data_set': 'all' }
+#'coef':[{'0': 0.1, '1': 0.2, '2': -0.1}]
     },
     organization_ids=[org_ids[0]]
 )
 
 results = client.get_results(average_task.get("id"))
 print(results)
+df_km = results[0]['kaplanMeier']
 try:
     results[0]['kaplanMeier'].to_csv('pippo.csv')
 except:
