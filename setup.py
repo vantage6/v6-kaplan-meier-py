509eba4351ce51b7fbb8a523eb54241401d01a0e
ivanzhovannik <ivan.zhovannik@medicaldataworks.nl>
2024-02-29 11:44:00 +0000

    cleaned setup.py

--- setup.py
+++ setup.py
@@ -2,24 +2,21 @@ from os import path
 from codecs import open
 from setuptools import setup, find_packages
 
-# we're using a README.md, if you do not have this in your folder, simply
-# replace this with a string.
-here = path.abspath(path.dirname(__file__))
-#with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-  #  long_description = f.read()
+# package description
+cwd = path.abspath(path.dirname(__file__))
+with open(path.join(cwd, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-# Here you specify the meta-data of your package. The `name` argument is
-# needed in some other steps.
 setup(
     name='vtg_km',
     version="1.0.0",
     description='vantage6 correlation matrix',
-   # long_description=long_description,
-   # long_description_content_type='text/markdown',
-  #  url='https://github.com/IKNL/v6-average-py',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mdw-nl/v6-km-studyathon',
     packages=find_packages(),
     python_requires='>=3.10',
     install_requires=[
-        'vantage6-algorithm-tools==4.2.0','numpy','pandas','scikit-learn'
+        'vantage6-algorithm-tools==4.2.0','numpy','pandas'
     ]
 )
