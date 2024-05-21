3457ca09e307e64c47d10e2a11746e0ed2111fda
F.C. Martin <f.martin@iknl.nl>
2024-05-21 15:05:01 +0200

    Renamed package, included `central.py`

--- v6-kaplan-meier-py/central.py
+++ v6-kaplan-meier-py/central.py
@@ -1,3 +1,11 @@
+"""
+This file contains all central algorithm functions. It is important to note
+that the central method is executed on a node, just like any other method.
+
+The results in a return statement are sent to the vantage6 server (after
+encryption if that is enabled).
+"""
+
 import sys
 
 from typing import Dict, List, Union
@@ -11,13 +19,13 @@ MINIMUM_ORGANIZATIONS = 3
 
 
 @algorithm_client
-def master(
+def central(
     client: AlgorithmClient,
     time_column_name: str,
     censor_column_name: str,
     bin_size: int = None,
     filter_value: str = None,
-    organization_ids: List[int] = None
+    organization_ids: List[int] = None,
 ) -> Dict[str, Union[str, List[str]]]:
     """Compute Kaplan-Meier curve in a federated environment.
 
@@ -32,7 +40,7 @@ def master(
     Returns:
     - Dictionary containing Kaplan-Meier curve and local event tables
     """
-    info('Collecting information on participating organizations')
+    info("Collecting information on participating organizations")
     if not isinstance(organization_ids, list):
         organizations = client.organization.list()
         ids = [organization.get("id") for organization in organizations]
@@ -40,16 +48,18 @@ def master(
         ids = organization_ids
 
     if len(ids) < MINIMUM_ORGANIZATIONS:
-        error(f"To further ensure privacy, a minimum of {MINIMUM_ORGANIZATIONS} participating organizations is required")
+        error(
+            f"To further ensure privacy, a minimum of {MINIMUM_ORGANIZATIONS} participating organizations is required"
+        )
         sys.exit(1)
 
-    info(f'Sending task to organizations {ids}')
+    info(f"Sending task to organizations {ids}")
     km = calculate_km(
         client=client,
         ids=ids,
         time_column_name=time_column_name,
         censor_column_name=censor_column_name,
         bin_size=bin_size,
-        filter_value=filter_value
+        filter_value=filter_value,
     )
     return km.to_json()
